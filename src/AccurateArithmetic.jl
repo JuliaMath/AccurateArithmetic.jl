b9ca315db74456a35de62238bdf2b0cdefde492c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 10:51:41 -0500

    cub

--- src/AccurateArithmetic.jl
+++ src/AccurateArithmetic.jl
@@ -2,7 +2,7 @@ module AccurateArithmetic
 
 export add_acc, add_hilo_acc, add₊, add_hilo₊, 
        sub_acc, sub_hilo_acc, sub_lohi_acc, sub₊, sub_hilo₊, sun_lohi₊,
-       sqr_acc, mul_acc, sqr₊, mul₊,
+       sqr_acc, mul_acc, cub_acc, sqr₊, mul₊, cub₊,
        inv_acc, div_acc, inv₊, div₊,
        sqrt_acc, sqrt₊
 
@@ -14,7 +14,7 @@ include("errorfree/addsubmul.jl")
 
 for (A, F) in ( (:add_acc, :(add₊)),  (:add_hilo_acc, :(add_hilo₊)),
                 (:sub_acc, :(sub₊)),  (:sub_hilo_acc, :(sub_hilo₊)), (:sub_lohi_acc, :(sub_lohi₊)),
-                (:sqr_acc, :(sqr₊)),  (:mul_acc, :(mul₊)),
+                (:sqr_acc, :(sqr₊)),  (:mul_acc, :(mul₊)), (:cub_acc, :(cub₊)),
                 (:inv_acc, :(inv₊)),  (:div_acc, :(div₊)),
                 (:sqrt_acc, :(sqrt₊)), )
    @eval begin
