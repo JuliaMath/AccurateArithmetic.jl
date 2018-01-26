e86834ed61dfeb1498c4a0c826530b666196f0e4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 10:26:31 -0500

    add four args

--- src/errorfree/addsub.jl
+++ src/errorfree/addsub.jl
@@ -118,3 +118,21 @@ function sub_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
     x, y = add_hilo_acc(x, y)
     return x, y, z
 end
+
+
+"""
+    add_acc(a, b, c, d)
+    
+Computes `s = fl(a+b+c+d)` and `e1, e2, e3 = err(a+b+c+d)`.
+"""
+function add_acc(a::T,b::T,c::T,d::T) where T<: AbstractFloat
+
+    t0, t1 = add_acc(a ,  b)
+    t0, t2 = add_acc(t0,  c)
+    a,  t3 = add_acc(t0,  d)
+    t0, t1 = add_acc(t1, t2)
+    b,  t2 = add_acc(t0, t3)
+    c,  d  = add_acc(t1, t2)
+
+    return a, b, c, d
+end
