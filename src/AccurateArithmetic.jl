e3440cd72d067bd75606cd48f1bdd89879acdc3d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 09:36:40 -0500

    function name ASCII synonyms

--- src/AccurateArithmetic.jl
+++ src/AccurateArithmetic.jl
@@ -8,5 +8,15 @@ export add_acc, add_acc_hilo, add₊, add_hilo₊,
 
 import Base.IEEEFloat # Union{Float64, Float32, Float16}
 
+for (A, F) in ( (:add_acc, :(add₊)),  (:add_acc_hilo, :(add_hilo₊)),
+                (:sub_acc, :(sub₊)),  (:sub_acc_hilo, :(sub_hilo₊)), (:sub_acc_lohi, :(sub_lohi₊)),
+                (:sqr_acc, :(sqr₊)),  (:mul_acc, :(mul₊)),
+                (:inv_acc, :(inv₊)),  (:div_acc, :(div₊)),
+                (:sqrt_acc, :(sqrt₊)), )
+   @eval begin
+       const $F = $A
+   end
+end
+
 
 end # module AccurateArithmetic
