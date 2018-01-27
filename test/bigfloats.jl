76f480665e6b1eaa0ddbbae60eee3dd2ea25ceb6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 23:58:30 -0500

    Create bigfloats.jl

---
+++ test/bigfloats.jl
@@ -0,0 +1,99 @@
+# use BigFloats to determine the correct multipart significand values
+
+setprecison(BigFloat, 1024-32)
+
+const Floats1 = NTuple{1,T} where {T<:AbstractFloat}
+const Floats2 = NTuple{2,T} where {T<:AbstractFloat}
+const Floats3 = NTuple{3,T} where {T<:AbstractFloat}
+const Floats4 = NTuple{4,T} where {T<:AbstractFloat}
+const Floats5 = NTuple{5,T} where {T<:AbstractFloat}
+const Floats6 = NTuple{6,T} where {T<:AbstractFloat}
+const Floats7 = NTuple{7,T} where {T<:AbstractFloat}
+const Floats8 = NTuple{8,T} where {T<:AbstractFloat}
+const Floats9 = NTuple{9,T} where {T<:AbstractFloat}
+
+
+function Floats1(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   return (fl1, )
+end
+
+function Floats2(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   return fl1, fl2
+end
+
+function Floats3(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   return fl1, fl2, fl3
+end
+
+function Floats4(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   return fl1, fl2, fl3, fl4
+end
+
+function Floats5(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   fl5 = T(bf - fl1 - fl2 - fl3 - fl4)
+   return fl1, fl2, fl3, fl4, fl5
+end
+
+function Floats6(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   fl5 = T(bf - fl1 - fl2 - fl3 - fl4)
+   fl6 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5)
+   return fl1, fl2, fl3, fl4, fl5, fl6
+end
+
+function Floats7(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   fl5 = T(bf - fl1 - fl2 - fl3 - fl4)
+   fl6 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5)
+   fl7 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6)
+   return fl1, fl2, fl3, fl4, fl5, fl6, fl7
+end
+
+function Floats8(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   fl5 = T(bf - fl1 - fl2 - fl3 - fl4)
+   fl6 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5)
+   fl7 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6)
+   fl8 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6 - fl7)
+   return fl1, fl2, fl3, fl4, fl5, fl6, fl7, fl8
+end
+
+function Floats9(::Type{T}, bf::BigFloat) where {T<:AbstractFloat}
+   fl1 = T(bf)
+   fl2 = T(bf - fl1)
+   fl3 = T(bf - fl1 - fl2)
+   fl4 = T(bf - fl1 - fl2 - fl3)
+   fl5 = T(bf - fl1 - fl2 - fl3 - fl4)
+   fl6 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5)
+   fl7 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6)
+   fl8 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6 - fl7)
+   fl9 = T(bf - fl1 - fl2 - fl3 - fl4 - fl5 - fl6 - fl7 - fl8)
+   return fl1, fl2, fl3, fl4, fl5, fl6, fl7, fl8, fl9
+end
+
+
+
+Base.convert(::Type{T}, bf::BigFloat) where T<:AbstractFloat
