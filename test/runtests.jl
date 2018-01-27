ca7ecb26866e10b81b90b801b82724b7f0487d90
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-27 18:23:20 -0500

    test fma, fms, sum

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,12 +17,14 @@ const fl32s = randfl(Float32, nrands)
 hi64, lo64 = Floats2(Float64, flbig[1])
 hi32, lo32 = Floats2(Float32, flbig[2])
 
-@test add_acc(hi64, lo64) == (hi64, lo64)
+@test add_acc(lo64, hi64) == (hi64, lo64)
 @test add_hilo_acc(hi32, lo32) == (hi32, lo32)
+
+@test sub_acc(lo64, hi64) == Floats2(Float64, BigFloat(lo64) - BigFloat(hi64))
+@test sub_hilo_acc(hi32, lo32) == Floats2(Float64, BigFloat(hi64) - BigFloat(lo64))
 
 hi64, lo64 = Floats2(Float64, flbig[3])
 hi32, lo32 = Floats2(Float32, flbig[4])
-hi, md, lo = Floats3(Float64, flbig[5])
 
 @test sqr_acc(hi64) == Floats2(Float64, BigFloat(hi64)^2)
 @test cub_acc(hi32) == Floats2(Float32, BigFloat(hi32)^3)
@@ -33,7 +35,10 @@ hi, md, lo = Floats3(Float64, flbig[5])
 @test sqrt_acc(hi64) == Floats2(Float64, sqrt(BigFloat(hi64)))
 @test inv_acc(hi32)  == Floats2(Float32, inv(BigFloat(hi32)))
 
-@test fma_acc(hi, lo, md) == Floats3(BigFloat(hi) * BigFloat(lo) + BigFloat(md))
-@test fms_acc(md, lo, hi) == Floats3(BigFloat(md) * BigFloat(lo) + BigFloat(hi))
+hi64, md64, lo64 = Floats3(Float64, flbig[5])
+hi32, md32, lo32 = Floats3(Float32, flbig[5])
+
+@test fma_acc(hi64, lo64, md64) == Floats3(BigFloat(hi64) * BigFloat(lo64) + BigFloat(md64))
+@test fms_acc(md32, lo32, hi32) == Floats3(BigFloat(md21) * BigFloat(lo32) + BigFloat(hi32))
 
 @test sum_acc(fl64s) == Floats2(sum(flbig))
