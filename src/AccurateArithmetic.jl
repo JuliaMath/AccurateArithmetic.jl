b005719810b7b93bb73a24cb1bc32f9a01b503f5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 22:11:52 -0500

    sum

--- src/AccurateArithmetic.jl
+++ src/AccurateArithmetic.jl
@@ -5,7 +5,7 @@ export add_acc, add₊, sub_acc, sub₊,
        sub_hilo_acc, sub_hilo₊, sub_lohi_acc, sub_lohi₊,
        sqr_acc, sqr₊, mul_acc, mul₊, cub_acc, cub₊,
        inv_acc, inv₊, div_acc, div₊, sqrt_acc, sqrt₊,
-       fma_acc, fma₊
+       fma_acc, fma₊, sum_acc, sum₊
 
 
 import Base.IEEEFloat # Union{Float64, Float32, Float16}
@@ -13,13 +13,15 @@ import Base.IEEEFloat # Union{Float64, Float32, Float16}
 
 include("errorfree/addsubmul.jl")
 include("faithful/divsqrt.jl")
+include("compensated/sum.jl")
 
 
 for (A, F) in ( (:add_acc, :(add₊)),  (:add_hilo_acc, :(add_hilo₊)),
                 (:sub_acc, :(sub₊)),  (:sub_hilo_acc, :(sub_hilo₊)), (:sub_lohi_acc, :(sub_lohi₊)),
                 (:sqr_acc, :(sqr₊)),  (:mul_acc, :(mul₊)), (:cub_acc, :(cub₊)),
                 (:inv_acc, :(inv₊)),  (:div_acc, :(div₊)),
-                (:sqrt_acc, :(sqrt₊)), (:fma_acc, :(fma₊)),  )
+                (:sqrt_acc, :(sqrt₊)), (:fma_acc, :(fma₊)),
+                (:sum_acc, :(sum₊)), )
    @eval begin
        const $A = $F
    end
