6a70baca4ec4b00d6ac6e4bf7273ed1eb771dd19
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-27 10:31:27 -0500

    reorg docs

--- src/errorfree/addsubmul.jl
+++ src/errorfree/addsubmul.jl
@@ -1,21 +1,3 @@
-"""
-    add_hilo_acc(a, b)
-    
-*unchecked* requirement `|a| ≥ |b|`
-
-Computes `s = fl(a+b)` and `e = err(a+b)`.
-"""
-@inline function add_hilo_acc(a::T, b::T) where {T<:AbstractFloat}
-    s = a + b
-    e = b - (s - a)
-    return s, e
-end
-
-"""
-    add_acc(a, b)
-    
-Computes `s = fl(a+b)` and `e = err(a+b)`.
-"""
 @inline function add_acc(a::T, b::T) where {T<:AbstractFloat}
     s = a + b
     v = s - a
@@ -23,18 +5,69 @@ Computes `s = fl(a+b)` and `e = err(a+b)`.
     return s, e
 end
 
-"""
-    sub_hilo_acc(a, b)
-    
-*unchecked* requirement `|a| ≥ |b|`
+function add_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = add_acc(b, c)
+    x, u = add_acc(a, s)
+    y, z = add_acc(u, t)
+    x, y = add_hilo_acc(x, y)
+    return x, y, z
+end
 
-Computes `s = fl(a-b)` and `e = err(a-b)`.
-"""
+function add_acc(a::T,b::T,c::T,d::T) where T<: AbstractFloat
+    t0, t1 = add_acc(a ,  b)
+    t0, t2 = add_acc(t0,  c)
+    a,  t3 = add_acc(t0,  d)
+    t0, t1 = add_acc(t1, t2)
+    b,  t2 = add_acc(t0, t3)
+    c,  d  = add_acc(t1, t2)
+    return a, b, c, d
+end
+
+
+@inline function sub_acc(a::T, b::T) where {T<:AbstractFloat}
+    s = a - b
+    v = s - a
+    e = (a - (s - v)) - (b + v)
+    return s, e
+end
+
+function sub_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = sub_acc(-b, c)
+    x, u = add_acc(a, s)
+    y, z = add_acc(u, t)
+    x, y = add_hilo_acc(x, y)
+    return x, y, z
+end
+
+@inline function add_hilo_acc(a::T, b::T) where {T<:AbstractFloat}
+    s = a + b
+    e = b - (s - a)
+    return s, e
+end
+
+function add_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = add_hilo_acc(b, c)
+    x, u = add_hilo_acc(a, s)
+    y, z = add_hilo_acc(u, t)
+    x, y = add_hilo_acc(x, y)
+    return x, y, z
+end
+
 @inline function sub_hilo_acc(a::T, b::T) where {T<:AbstractFloat}
     s = a - b
     e = (a - s) - b
     s, e
 end
+
+function sub_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = sub_hilo_acc(-b, c)
+    x, u = add_hilo_acc(a, s)
+    y, z = add_hilo_acc(u, t)
+    x, y = add_hilo_acc(x, y)
+    return x, y, z
+end
+
+
 
 """
     sub_lohi_acc(a, b)
@@ -47,94 +80,6 @@ Computes `s = fl(a-b)` and `e = err(a-b)`.
     s = a - b
     e = (s - b) + a
     s, e
-end
-
-"""
-    sub_acc(a, b)
-    
-Computes `s = fl(a-b)` and `e = err(a-b)`.
-"""
-@inline function sub_acc(a::T, b::T) where {T<:AbstractFloat}
-    s = a - b
-    v = s - a
-    e = (a - (s - v)) - (b + v)
-
-    s, e
-end
-
-
-"""
-    add_hilo_acc(a, b, c)
-    
-*unchecked* requirement `|a| ≥ |b| ≥ |c|`
-
-Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
-"""
-function add_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = add_hilo_acc(b, c)
-    x, u = add_hilo_acc(a, s)
-    y, z = add_hilo_acc(u, t)
-    x, y = add_hilo_acc(x, y)
-    return x, y, z
-end
-
-"""
-    add_acc(a, b, c)
-    
-Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
-"""
-function add_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = add_acc(b, c)
-    x, u = add_acc(a, s)
-    y, z = add_acc(u, t)
-    x, y = add_hilo_acc(x, y)
-    return x, y, z
-end
-
-"""
-    sub_hilo_acc(a, b, c)
-    
-*unchecked* requirement `|a| ≥ |b| ≥ |c|`
-
-Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
-"""
-function sub_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = sub_hilo_acc(-b, c)
-    x, u = add_hilo_acc(a, s)
-    y, z = add_hilo_acc(u, t)
-    x, y = add_hilo_acc(x, y)
-    return x, y, z
-end
-
-"""
-    sub_acc(a, b, c)
-    
-Computes `s = fl(a-b-c)` and `e1 = err(a-b-c), e2 = err(e1)`.
-"""
-function sub_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = sub_acc(-b, c)
-    x, u = add_acc(a, s)
-    y, z = add_acc(u, t)
-    x, y = add_hilo_acc(x, y)
-    return x, y, z
-end
-
-
-"""
-    add_acc(a, b, c, d)
-    
-Computes `s = fl(a+b+c+d)` and `e1, e2, e3 = err(a+b+c+d)`.
-"""
-function add_acc(a::T,b::T,c::T,d::T) where T<: AbstractFloat
-
-    t0, t1 = add_acc(a ,  b)
-    t0, t2 = add_acc(t0,  c)
-    a,  t3 = add_acc(t0,  d)
-    t0, t1 = add_acc(t1, t2)
-    b,  t2 = add_acc(t0, t3)
-    c,  d  = add_acc(t1, t2)
-
-    return a, b, c, d
 end
 
 
@@ -198,3 +143,49 @@ function fma_acc(a::T, b::T, c::T) where {T<:AbstractFloat}
     y, z = add_hilo_acc(y, z)
     return x, y, z
 end
+
+"""
+    add_acc(a, b, { c, { d }})
+
+accurate addition with 2, 3, or 4 floats
+
+returns the same number of floats,
+transformed to separate significands
+as a tuple of decreasing magnitudes
+""" add_acc
+
+"""
+    add_hilo_acc(a, b, { c })
+    
+*unchecked* `|a| ≥ |b|` `{|b| ≥ |c| }`
+
+accurate addition with 2 or 3 floats
+given in order of nonincreasing magnitude
+
+returns the same number of floats,
+transformed to separate significands
+as a tuple of decreasing magnitudes
+""" add_hilo_acc
+
+"""
+    sub_acc(a, b, { c, { d }})
+
+accurate subtraction with 2, 3, or 4 floats
+
+returns the same number of floats,
+transformed to separate significands
+as a tuple of decreasing magnitudes
+""" sub_acc
+
+"""
+    sub_hilo_acc(a, b, { c })
+    
+*unchecked* requirement `|a| ≥ |b|` {`|b| ≥ |c|`}
+
+accurate subtraction with 2 or 3 floats
+given in order of nonincreasing magnitude
+
+returns the same number of floats,
+transformed to separate significands
+as a tuple of decreasing magnitudes
+""" sub_hilo_acc
