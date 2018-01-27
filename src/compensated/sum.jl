f539d85179614ad2c032390df44c17c70184f287
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 22:00:45 -0500

    Create sum.jl

---
+++ src/compensated/sum.jl
@@ -0,0 +1,23 @@
+function sum_acc(x::A) where {T, N, A<:AbstractArray{T,N}}
+    n = length(x)
+    if n < 2 
+        iszero(n) && return zero(T)
+        isone(n)  && return x[1]
+    end
+    return do_sum_acc(n, x)
+end
+
+function do_sum_acc(n::Int, x::A) where {T, N, A<:AbstractArray{T,N}}
+   hi = x[1]
+   lo = zero(T)
+   for i in 2:n
+       hi, low = add_acc(hi, x[i])
+       lo += low
+   end
+   hi, lo = add_hilo_acc(hi, lo)
+   return hi, lo
+end
+
+       
+   
+   
