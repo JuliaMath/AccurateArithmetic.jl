2c9cfd4e76a4db8dd0db389f6e7dab9e1e6f5133
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-29 08:18:18 -0500

    hilo

--- src/compensated.jl
+++ src/compensated.jl
@@ -1,13 +1,13 @@
-function sum_acc(x::A) where {T, N, A<:AbstractArray{T,N}}
+function sum_hilo(x::A) where {T, N, A<:AbstractArray{T,N}}
     n = length(x)
     if n < 2 
         iszero(n) && return zero(T)
         isone(n)  && return x[1]
     end
-    return do_sum_acc(n, x)
+    return do_sum_hilo(n, x)
 end
 
-function do_sum_acc(n::Int, x::A) where {T, N, A<:AbstractArray{T,N}}
+function do_sum_hilo(n::Int, x::A) where {T, N, A<:AbstractArray{T,N}}
    hi = x[1]
    lo = zero(T)
    for i in 2:n
@@ -17,6 +17,7 @@ function do_sum_acc(n::Int, x::A) where {T, N, A<:AbstractArray{T,N}}
    hi, lo = add_hilo_acc(hi, lo)
    return hi, lo
 end
+
 
        
    
