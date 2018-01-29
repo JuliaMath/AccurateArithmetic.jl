ada0a28d760b73cf8b91d2fcbf3e4178c9363570
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-29 09:16:20 -0500

    rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,32 +17,32 @@ const fl32s = map(Float32, flbig)
 hi64, lo64 = Floats2(Float64, flbig[1])
 hi32, lo32 = Floats2(Float32, flbig[2])
 
-@test add_acc(lo64, hi64) == (hi64, lo64)
-@test add_hilo_acc(hi32, lo32) == (hi32, lo32)
+@test add_hilo(lo64, hi64) == (hi64, lo64)
+@test add_hilo_hilo(hi32, lo32) == (hi32, lo32)
 
-@test sub_acc(lo64, hi64) == Floats2(Float64, BigFloat(lo64) - BigFloat(hi64))
-@test sub_hilo_acc(hi32, lo32) == Floats2(Float32, BigFloat(hi32) - BigFloat(lo32))
+@test sub_hilo(lo64, hi64) == Floats2(Float64, BigFloat(lo64) - BigFloat(hi64))
+@test sub_hilo_hilo(hi32, lo32) == Floats2(Float32, BigFloat(hi32) - BigFloat(lo32))
 
 hi64, lo64 = Floats2(Float64, flbig[3])
 hi32, lo32 = Floats2(Float32, flbig[4])
 
-@test sqr_acc(hi64) == Floats2(Float64, BigFloat(hi64)^2)
-@test cub_acc(hi32) == Floats2(Float32, BigFloat(hi32)^3)
+@test sqr_hilo(hi64) == Floats2(Float64, BigFloat(hi64)^2)
+@test cub_hilo(hi32) == Floats2(Float32, BigFloat(hi32)^3)
 
-@test mul_acc(hi64, lo64) == Floats2(Float64, BigFloat(hi64)*BigFloat(lo64))
-@test div_acc(hi32, lo32) == Floats2(Float32, BigFloat(hi32)/BigFloat(lo32))
+@test mul_hilo(hi64, lo64) == Floats2(Float64, BigFloat(hi64)*BigFloat(lo64))
+@test div_hilo(hi32, lo32) == Floats2(Float32, BigFloat(hi32)/BigFloat(lo32))
 
-@test sqrt_acc(hi64) == Floats2(Float64, sqrt(BigFloat(hi64)))
-@test inv_acc(hi32)  == Floats2(Float32, inv(BigFloat(hi32)))
+@test sqrt_hilo(hi64) == Floats2(Float64, sqrt(BigFloat(hi64)))
+@test inv_hilo(hi32)  == Floats2(Float32, inv(BigFloat(hi32)))
 
 hi64, md64, lo64 = Floats3(Float64, flbig[5])
 hi32, md32, lo32 = Floats3(Float32, flbig[5])
 
-@test mul_acc(hi64, lo64, md64) == Floats4(Float64, BigFloat(hi64) * BigFloat(lo64) * BigFloat(md64))
-@test mul3acc(hi64, lo64, md64) == Floats3(Float64, BigFloat(hi64) * BigFloat(lo64) * BigFloat(md64))
+@test mul_hilo(hi64, lo64, md64) == Floats4(Float64, BigFloat(hi64) * BigFloat(lo64) * BigFloat(md64))
+@test mul_hilo3(hi64, lo64, md64) == Floats3(Float64, BigFloat(hi64) * BigFloat(lo64) * BigFloat(md64))
 
-@test fma_acc(hi64, lo64, md64) == Floats3(Float64, BigFloat(hi64) * BigFloat(lo64) + BigFloat(md64))
-@test fms_acc(md32, lo32, hi32) == Floats3(Float32, BigFloat(md32) * BigFloat(lo32) - BigFloat(hi32))
+@test fma_hilo(hi64, lo64, md64) == Floats3(Float64, BigFloat(hi64) * BigFloat(lo64) + BigFloat(md64))
+@test fms_hilo(md32, lo32, hi32) == Floats3(Float32, BigFloat(md32) * BigFloat(lo32) - BigFloat(hi32))
 
 flbig64s = map(BigFloat, fl64s)
-@test sum_acc(fl64s) == Floats2(Float64, sum(flbig64s))
+@test sum_hilo(fl64s) == Floats2(Float64, sum(flbig64s))
