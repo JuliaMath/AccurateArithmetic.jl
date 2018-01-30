05495d23ae30b3d99eb87e6e417447fe02f8fd46
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-30 14:08:49 -0500

    fast_two_sum two_sum two_prod

--- src/renormalize.jl
+++ src/renormalize.jl
@@ -1,9 +1,22 @@
-function renorm_inorder(a::T, b::T) where T<:Real
+function fast_two_sum(a::T, b::T) where T<:Real
      @confirm abs(a) > abs(b)
      
      ab = a + b
      ε  = (a - ab) + b
      return ab, ε 
+end
+
+function two_sum(a::T, b::T) where T<:Real
+    ab =  a + b
+    B  = ab - a
+    ε  = (a - (ab - B)) + (b - B)
+    return ab, ε
+end
+
+function two_prod(a::T, b::T) where T<:Real
+    ab =  a * b
+    ε  = fma(a, b, -ab)
+    return ab, ε
 end
 
 
