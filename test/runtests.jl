19d679956337eb8bc75a6a1aa5a78b527e4a22d6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-30 06:29:57 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,9 +30,9 @@ hi32, lo32 = Floats2(Float32, flbig[4])
 @test cub_acc(hi32) == Floats2(Float32, BigFloat(hi32)^3)
 
 @test mul_acc(hi64, lo64) == Floats2(Float64, BigFloat(hi64)*BigFloat(lo64))
-@test xdiv_acc(hi32, lo32) == Floats2(Float32, BigFloat(hi32)/BigFloat(lo32))
+@test div_acc(hi32, lo32) == Floats2(Float32, BigFloat(hi32)/BigFloat(lo32))
 
-@test xsqrt(hi64) == Floats2(Float64, xsqrt(BigFloat(hi64)))
+@test sqrt(hi64) == Floats2(Float64, xsqrt(BigFloat(hi64)))
 @test inv_acc(hi32)  == Floats2(Float32, inv(BigFloat(hi32)))
 
 hi64, md64, lo64 = Floats3(Float64, flbig[5])
