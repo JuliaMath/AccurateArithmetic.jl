aaa2ddbeb632c7c143f13875cc228622bb3febd0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-31 08:44:43 -0500

    remove  @confirm

--- src/AccurateArithmetic.jl
+++ src/AccurateArithmetic.jl
@@ -13,20 +13,8 @@ if VERSION >= v"0.7.0-"
     import Base.IEEEFloat
 else
     const IEEEFloat = Union{Float64, Float32, Float16}
-    macro isdefined(x)
-        :(isdefined(Symbol($x)))
-    end
 end
 
-if @isdefined CONFIRM
-   if !@isdefined confirm
-       macro confirm(expr)
-           :(@assert $expr)
-       end
-   end  
-else
-    macro confirm(expr) end
-end
 
 include("renormalize.jl")
 include("errorfree.jl")
