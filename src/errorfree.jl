f7a4f125ef1fa35d76ce74581646a7a7daf228c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-31 10:31:18 -0500

    rename

--- src/errorfree.jl
+++ src/errorfree.jl
@@ -1,5 +1,5 @@
 # this is "TwoSum"
-@inline function add_acc(a::T, b::T) where {T<:AbstractFloat}
+@inline function add_(a::T, b::T) where {T<:AbstractFloat}
     s = a + b
     v = s - a
     e = (a - (s - v)) + (b - v)
@@ -7,112 +7,112 @@
 end
 
 # ThreeSum
-function add_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = add_acc(b, c)
-    x, u = add_acc(a, s)
-    y, z = add_acc(u, t)
-    x, y = add_hilo_acc(x, y)
+function add_(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = add_(b, c)
+    x, u = add_(a, s)
+    y, z = add_(u, t)
+    x, y = add_hilo_(x, y)
     return x, y, z
 end
 
 # FourSum
-function add_acc(a::T,b::T,c::T,d::T) where {T<: AbstractFloat}
-    t0, t1 = add_acc(a ,  b)
-    t0, t2 = add_acc(t0,  c)
-    a,  t3 = add_acc(t0,  d)
-    t0, t1 = add_acc(t1, t2)
-    b,  t2 = add_acc(t0, t3)
-    c,  d  = add_hilo_acc(t1, t2)
+function add_(a::T,b::T,c::T,d::T) where {T<: AbstractFloat}
+    t0, t1 = add_(a ,  b)
+    t0, t2 = add_(t0,  c)
+    a,  t3 = add_(t0,  d)
+    t0, t1 = add_(t1, t2)
+    b,  t2 = add_(t0, t3)
+    c,  d  = add_hilo_(t1, t2)
     return a, b, c, d
 end
 
 # this is TwoDiff
-@inline function sub_acc(a::T, b::T) where {T<:AbstractFloat}
+@inline function sub_(a::T, b::T) where {T<:AbstractFloat}
     s = a - b
     v = s - a
     e = (a - (s - v)) - (b + v)
     return s, e
 end
 
-function sub_acc(a::T, b::T, c::T) where {T<:AbstractFloat}
-    s, t = sub_acc(-b, c)
-    x, u = add_acc(a, s)
-    y, z = add_acc(u, t)
-    x, y = add_hilo_acc(x, y)
+function sub_(a::T, b::T, c::T) where {T<:AbstractFloat}
+    s, t = sub_(-b, c)
+    x, u = add_(a, s)
+    y, z = add_(u, t)
+    x, y = add_hilo(x, y)
     return x, y, z
 end
 
 # this is QuickTwoSum, requires abs(a) >= abs(b)
-@inline function add_hilo_acc(a::T, b::T) where {T<:AbstractFloat}
+@inline function add_hilo_(a::T, b::T) where {T<:AbstractFloat}
     s = a + b
     e = b - (s - a)
     return s, e
 end
 
-function add_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = add_hilo_acc(b, c)
-    x, u = add_hilo_acc(a, s)
-    y, z = add_hilo_acc(u, t)
-    x, y = add_hilo_acc(x, y)
+function add_hilo_(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = add_hilo_(b, c)
+    x, u = add_hilo_(a, s)
+    y, z = add_hilo_(u, t)
+    x, y = add_hilo_(x, y)
     return x, y, z
 end
 
 # this is QuickTwoDiff, requires abs(a) >= abs(b)
-@inline function sub_hilo_acc(a::T, b::T) where {T<:AbstractFloat}
+@inline function sub_hilo_(a::T, b::T) where {T<:AbstractFloat}
     s = a - b
     e = (a - s) - b
     s, e
 end
 
-function sub_hilo_acc(a::T,b::T,c::T) where {T<:AbstractFloat}
-    s, t = sub_hilo_acc(-b, c)
-    x, u = add_hilo_acc(a, s)
-    y, z = add_hilo_acc(u, t)
-    x, y = add_hilo_acc(x, y)
+function sub_hilo_(a::T,b::T,c::T) where {T<:AbstractFloat}
+    s, t = sub_hilo_(-b, c)
+    x, u = add_hilo_(a, s)
+    y, z = add_hilo_(u, t)
+    x, y = add_hilo_(x, y)
     return x, y, z
 end
 
 # this is TwoProdFMA
-@inline function mul_acc(a::T, b::T) where {T<:AbstractFloat}
+@inline function mul_(a::T, b::T) where {T<:AbstractFloat}
     p = a * b
     e = fma(a, b, -p)
     return p, e
 end
 
-function mul_acc(a::T, b::T, c::T) where {T<:AbstractFloat}
-    y, z = mul_acc(a, b)
-    x, y = mul_acc(y, c)
-    z, t = mul_acc(z, c)
+function mul_(a::T, b::T, c::T) where {T<:AbstractFloat}
+    y, z = mul_(a, b)
+    x, y = mul_(y, c)
+    z, t = mul_(z, c)
     return x, y, z, t
 end
 
 """
-    mul_acc3(a, b, c)
+    mul_3(a, b, c)
 
-similar to mul_acc(a, b, c)
+similar to mul_(a, b, c)
 returns a three tuple
 """
-function mul_acc3(a::T, b::T, c::T) where {T<:AbstractFloat}
-    y, z = mul_acc(a, b)
-    x, y = mul_acc(y, c)
+function mul_3(a::T, b::T, c::T) where {T<:AbstractFloat}
+    y, z = mul_(a, b)
+    x, y = mul_(y, c) 
     z    *= c
     return x, y, z
 end
 
 # a squared
-@inline function sqr_acc(a::T) where {T<:AbstractFloat}
+@inline function sqr_(a::T) where {T<:AbstractFloat}
     p = a * a
     e = fma(a, a, -p)
     return p, e
 end
 
 # a cubed
-@inline function cub_acc(a::T) where {T<:AbstractFloat}
-    hi, lo = sqr_acc(a)
-    hihi, _hilo = mul_acc(hi, a)
-    lohi, lolo = mul_acc(lo, a)
-    _hilo, lohi = add_hilo_acc(_hilo, lohi)
-    hi, lo = add_hilo_acc(hihi, _hilo)
+@inline function cub_(a::T) where {T<:AbstractFloat}
+    hi, lo = sqr_(a)
+    hihi, _hilo = mul_(hi, a)
+    lohi, lolo = mul_(lo, a)
+    _hilo, lohi = add_hilo_(_hilo, lohi)
+    hi, lo = add_hilo_(hihi, _hilo)
     lo += lohi + lolo
     return hi, lo
 end
@@ -124,25 +124,25 @@ end
 =#
 
 """
-    fma_acc(a, b, c) => (x, y, z)
+    fma_(a, b, c) => (x, y, z)
 
-Computes `x = fl(fma_acc(a, b, c))` and `y, z = fl(err(fma_acc(a, b, c)))`.
+Computes `x = fl(fma_(a, b, c))` and `y, z = fl(err(fma_(a, b, c)))`.
 """
-function fma_acc(a::T, b::T, c::T) where {T<:AbstractFloat}
+function fma_(a::T, b::T, c::T) where {T<:AbstractFloat}
      x = fma(a, b, c)
-     y, z = mul_acc(a, b)
-     t, z = add_acc(c, z)
-     t, u = add_acc(y, t)
+     y, z = mul_(a, b)
+     t, z = add_(c, z)
+     t, u = add_(y, t)
      y = ((t - x) + u)
-     y, z = add_hilo_acc(y, z)
+     y, z = add_hilo_(y, z)
      return x, y, z
 end
 
 """
-    fms_acc(a, b, c) => (x, y, z)
+    fms_(a, b, c) => (x, y, z)
 
-Computes `x = fl(fms_acc(a, b, c))` and `y, z = fl(err(xfms_acc(a, b, c)))`.
+Computes `x = fl(fms_(a, b, c))` and `y, z = fl(err(fms_(a, b, c)))`.
 """
-@inline function fms_acc(a::T, b::T, c::T) where {T<:AbstractFloat}
-     return fma_acc(a, b, -c)
+@inline function fms_(a::T, b::T, c::T) where {T<:AbstractFloat}
+     return fma_(a, b, -c)
 end
