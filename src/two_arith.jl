1fc49bc36c0f2d208c6d02050ad33608a997c9da
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-31 11:16:48 -0500

    Create two_arith.jl

---
+++ src/two_arith.jl
@@ -0,0 +1,53 @@
+
+
+function two_sum(a::T, b::T) where T<:Real
+    ab =  a + b
+    B  = ab - a
+    ε  = (a - (ab - B)) + (b - B)
+    return ab, ε
+end
+
+"""
+    fast_two_sum(x, y)::(a, b)
+
+|  ab  =  a + b
+|  ε   = (a - ab) + b
+
+"""
+@inline function fast_two_sum(a::T, b::T) where T<:Real
+     @confirm abs(a) > abs(b)
+     
+     ab = a + b
+     ε  = a + b - ab
+     return ab, ε
+end
+
+function two_diff(a::T, b::T) where T<:Real
+    ab =  a - b
+    B  = ab - a
+    ε  = (a - (ab - B)) + (b - B)
+    return ab, ε
+end
+
+
+"""
+    fast_two_diff(x, y)::(a, b)
+
+|  ab  =  a + b
+|  ε   = (a - ab) + b
+
+"""
+@inline function fast_two_diff(a::T, b::T) where T<:Real
+     @confirm abs(a) > abs(b)
+     
+     ab = a + b
+     ε  = a + b - ab
+     return ab, ε
+end
+
+
+function two_prod(a::T, b::T) where T<:Real
+    ab =  a * b
+    ε  = fma(a, b, -ab)
+    return ab, ε
+end
