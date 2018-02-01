f038e9da25c64c66b2a5f145124ccad7b172537f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-01 00:50:18 -0500

    Update twoarith.jl

--- src/twoarith.jl
+++ src/twoarith.jl
@@ -5,19 +5,6 @@ function two_sum(a::T, b::T) where T<:Real
     return ab, ε
 end
 
-"""
-    fast_two_sum(x, y)::(a, b)
-
-{ ab  =  a + b,  ε  = (a - ab) + b }
-
-__unchecked precondition__: abs(a) >= abs(b).
-"""
-@inline function fast_two_sum(a::T, b::T) where T<:Real
-     ab = a + b
-     ε  = a + b - ab
-     return ab, ε
-end
-
 function two_diff(a::T, b::T) where T<:Real
     ab =  a - b
     B  = ab - a
@@ -27,19 +14,32 @@ end
 
 
 """
+    fast_two_sum(x, y)::(a, b)
+
+{ ab  =  a + b,  ε  = (a - ab) + b }
+
+__unchecked precondition__: abs(a) >= abs(b).
+"""
+@inline function fast_two_sum(a::T, b::T) where T<:Real
+     ab =  a + b
+     B  = ab - a
+     ε  =  b - B
+     return ab, ε
+end
+
+"""
     fast_two_diff(x, y)::(a, b)
 
 { ab  =  a - b,  ε  = (a - ab) + b }
 
 __unchecked precondition__: abs(a) >= abs(b).
 """
-@inline function fast_two_diff(a::T, b::T) where T<:Real
-     
-     ab = a - b
-     ε  = a + b - ab
+@inline function fast_two_diff(a::T, b::T) where T<:Real     
+     ab =  a - b
+     B  = ab - a
+     ε  =  B - b
      return ab, ε
 end
-
 
 @inline function two_prod(a::T, b::T) where T<:Real
     ab =  a * b
