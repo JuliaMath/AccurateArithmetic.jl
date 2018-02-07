2243d0c284341ece26496da1be3ebb5e82601e65
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-07 00:14:50 -0500

    use significands in [0.5, 1)

--- test/randfloats.jl
+++ test/randfloats.jl
@@ -1,10 +1,18 @@
-# for significands in [1, 2)
+#= for significands in [1, 2)
 exponent_max(::Type{Float16})  =   15
 exponent_max(::Type{Float32})  =  127
 exponent_max(::Type{Float64})  = 1023
 exponent_min(::Type{Float16})  = 1 -   15
 exponent_min(::Type{Float32})  = 1 -  127
 exponent_min(::Type{Float64})  = 1 - 1023
+=#
+# for significands in [0.5, 1)
+exponent_max(::Type{Float16})  =   15+1
+exponent_max(::Type{Float32})  =  127+1
+exponent_max(::Type{Float64})  = 1023+1
+exponent_min(::Type{Float16})  = 2 -   15
+exponent_min(::Type{Float32})  = 2 -  127
+exponent_min(::Type{Float64})  = 2 - 1023
 
 for (U,F) in ((:UInt64, :Float64), (:UInt32, :Float32), (:UInt16, :Float16))
     @eval begin
@@ -20,16 +28,18 @@ for (U,F) in ((:UInt64, :Float64), (:UInt32, :Float32), (:UInt16, :Float16))
             emin = max(exponent_min($F), emin)
             emax = min(exponent_max($F), emax)
             n = max(1, n)
-            result = Vector{$F}(uninitialized, n)
+            result = rand($F, n)
+            # significands in [0.5, 1)
+            result *= 0.5
+            result += 0.5
+            exponent = rand(emin:1:emax, n)
             for i in 1:n
-                r = rand($F) + one($F) # significand in [1, 2)
-                e = rand(emin:1:emax)
-                result[i] = ldexp(r, e) 
+                result[i] = ldexp(result[i], exponent[i]) 
             end
             if signed
                signs = rand(-1:2:1,n)
                for i in 1:n
-                   result[i] = copysign(result[i], rand(-1:2:1))
+                   result[i] = copysign(result[i], signs[i])
                end
             end
             return n==1 ? result[1] : result
