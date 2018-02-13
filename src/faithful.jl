a39755ccca0a9e822de7f05262ac92941f20bc5e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-13 12:20:38 -0500

    conform to func_n pattern

--- src/faithful.jl
+++ src/faithful.jl
@@ -1,15 +1,12 @@
-"""
-    inv_(a)
-
-
-"""
-function inv_(b::T) where {T<:AbstractFloat}
+function inv_2(b::T) where {T<:AbstractFloat}
     hi = inv(b)
     v = hi * b
     w = fma(hi, b, -v)
     lo = (one(T) - v - w) / b
     return hi, lo
 end
+
+@inline inv_(b::T) where {T<:AbstractFloat} = inv_2(b)
 
 #=
 function div_(a::T, b::T) where {T<:AbstractFloat}
@@ -22,28 +19,21 @@ end
 =#
 # !?! `y` must be negated to get the right result
 
-"""
-    div_(a, b)
-
-Computes `q = fl(a/b)` and `e = err(a/b)`.
-"""
-function div_(a::T, b::T) where {T<:AbstractFloat}
-     q = a / b
-     e = -(fma(q, b, -a) / b)
-     return q, e
+function div_2(a::T, b::T) where {T<:AbstractFloat}
+     hi = a / b
+     lo = -(fma(hi, b, -a) / b)
+     return hi, lo
 end
 
-"""
-    sqrt_(a)
+@inline div_(a::T, b::T) where {T<:AbstractFloat} = div_2(b)
 
-Computes `r = fl(sqrt(a))` and `e = err(sqrt(a))`.
-"""
-function sqrt_(a::T) where {T<:AbstractFloat}
+function sqrt_2(a::T) where {T<:AbstractFloat}
     hi = sqrt(a)
     lo = fma(-hi, hi, a) / (hi + hi)
     return hi, lo
 end
 
+@inline sqrt_(a::T) where {T<:AbstractFloat} = sqrt_2(b)
 
 #=
 "Concerning the division, the elementary rounding error is
