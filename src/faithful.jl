1413e8a87507ad5f4ee52f43240f3bc3e8f5d363
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 01:41:44 -0500

    Update faithful.jl

--- src/faithful.jl
+++ src/faithful.jl
@@ -35,15 +35,6 @@ end
 
 @inline root2_(x::T) where {T<:AbstractFloat} = root2_2(x)
 
-function root3_2(x::T) where {T<:AbstractFloat}
-    hi = cbrt(x)
-    chi, clo = powr3_2(hi)
-    d = (-chi + x) - clo
-    lo = d / (T(3)*hi*hi)
-    return hi, lo
-end
-
-@inline root3_(x::T) where {T<:AbstractFloat} = root3_2(x)
 
 #=
 "Concerning the division, the elementary rounding error is
