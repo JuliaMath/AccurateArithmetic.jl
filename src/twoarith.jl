81272bae03ee79f7675721e7051c0d70bfbeb11a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 14:34:51 -0500

    Update twoarith.jl

--- src/twoarith.jl
+++ src/twoarith.jl
@@ -83,3 +83,21 @@ splitmax(::Type{Float16}) = realmax(Float16) / splitter(Float16)
     return zₕᵢ, zₗₒ
 end
 
+
+for (U,F) in ((:UInt64, :Float64), (:UInt32, :Float32), (:UInt16, :Float16))
+  @eval begin
+    @inline function ufp(x::$F)
+        u = reinterpret($U, x)
+        u = (u >> (precision($F)-1)) << (precision($F)-1)
+        return reinterpret($F, u)
+    end
+  end
+end
+
+const Float64ulp = inv(ldexp(1.0, precision(Float64)))
+const Float32ulp = inv(ldexp(1.0, precision(Float32)))
+const Float16ulp = inv(ldexp(1.0, precision(Float16)))
+
+@inline ulp(x::Float64) = ufp(x) * Float64ulp
+@inline ulp(x::Float32) = ufp(x) * Float32ulp
+@inline ulp(x::Float16) = ufp(x) * Float16ulp
