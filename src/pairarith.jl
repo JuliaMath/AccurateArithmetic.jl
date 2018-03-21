a70228584bdb3678d0911aad12c044778530a5eb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 11:26:26 -0400

    Update pairarith.jl

--- src/pairarith.jl
+++ src/pairarith.jl
@@ -1,20 +1,24 @@
-@inline function Base.:(-)(x::Tuple{T,T}) where T<:AbstractFloat
+@inline function signbit(x::Tuple{T,T}) where T<:AbstractFloat
+    return signbit(x[1])
+end
+
+@inline function (-)(x::Tuple{T,T}) where T<:AbstractFloat
     return -x[1], -x[2]
 end
 
-@inline function Base.abs(x::Tuple{T,T}) where T<:AbstractFloat
+@inline function abs(x::Tuple{T,T}) where T<:AbstractFloat
     return signbit(x[1]) ? -x : x
 end
 
-@inline function Base.flipsign(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
+@inline function flipsign(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     return signbit(y[1]) ? -x : x
 end
 
-@inline function Base.copysign(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
+@inline function copysign(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     return signbit(y[1]) ? -abs(x) : abs(x)
 end
 
-function Base.:(+)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
+function (+)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     hi, lo = two_sum(x[1], y[1])
     lohi, lolo = two_sum(x[2], y[2])
     lohi += lo
@@ -24,7 +28,7 @@ function Base.:(+)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     return hi, lo
 end
 
-function Base.:(-)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
+function (-)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     hi, lo = two_diff(x[1], y[1])
     lohi, lolo = two_diff(x[2], y[2])
     lohi += lo
@@ -34,7 +38,7 @@ function Base.:(-)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     return hi, lo
 end
 
-function Base.:(*)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
+function (*)(x::Tuple{T,T}, y::Tuple{T,T}) where T<:AbstractFloat
     lo = two_prod(x[2], y[2])
     hi = two_prod(x[1], y[2])
     hilo = lo + hi
