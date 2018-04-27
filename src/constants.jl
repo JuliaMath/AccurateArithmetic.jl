9387012c12d1c0277a997637a30ab7d0f3792a14
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-27 14:24:50 -0400

    Update constants.jl

--- src/constants.jl
+++ src/constants.jl
@@ -1,3 +1,7 @@
+four(::Type{Float64}) = 4.0
+four(::Type{Float32}) = 4.0f0
+four(::Type{Float16}) = Float16(4.0f0)
+
 two(::Type{Float64}) = 2.0
 two(::Type{Float32}) = 2.0f0
 two(::Type{Float16}) = Float16(2.0f0)
@@ -9,3 +13,7 @@ negone(::Type{Float16}) = Float16(-1.0f0)
 onehalf(::Type{Float64}) = 0.5
 onehalf(::Type{Float32}) = 0.5f0
 onehalf(::Type{Float16}) = Float16(0.5f0)
+
+onequarter(::Type{Float64}) = 0.25
+onequarter(::Type{Float32}) = 0.25f0
+onequarter(::Type{Float16}) = Float16(0.25f0)
