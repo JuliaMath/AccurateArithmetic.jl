7d137ab6fb8e404fe7070746262147dc1acf458e
François Févotte <ff@triscale-innov.com>
2020-06-22 15:44:37 +0200

    Dropped the dependency on MacroTools
    This also reduces (pre)compilation times

--- src/SIMDops.jl
+++ src/SIMDops.jl
@@ -1,5 +1,4 @@
 module SIMDops
-import MacroTools
 import SIMDPirates
 using  SIMDPirates: Vec, vload, vsum, vabs, vfma, vifelse, vless, vzero
 
@@ -12,7 +11,40 @@ fma(x::T, y::T, z::T) where {T<:NTuple} = SIMDPirates.vfma(x, y, z)
 fptype(::Type{Vec{W, T}}) where {W, T} = T
 
 
-# * Explicit/exact operations (non-fusible)
+# * Macros rewriting mathematical operations
+
+# ** Helper functions
+
+replace_simd_ops(x, _, _) = x
+
+function replace_simd_ops(sym::Symbol, ops, updates)
+    # Replace e.g:  +
+    #           =>  eadd
+    for (op, replacement) in ops
+        sym === op && return replacement
+    end
+    sym
+end
+
+function replace_simd_ops(expr::Expr, ops, updates)
+    # Replace e.g:  lhs += rhs
+    #           =>  lhs = eadd(lhs, rhs)
+    for (op, replacement) in updates
+        if expr.head === op
+            lhs = expr.args[1]
+            rhs = replace_simd_ops(expr.args[2], ops, updates)
+            return :($lhs = $replacement($lhs, $rhs))
+        end
+    end
+
+    newexpr = Expr(replace_simd_ops(expr.head, ops, updates))
+    for arg in expr.args
+        push!(newexpr.args, replace_simd_ops(arg, ops, updates))
+    end
+    newexpr
+end
+
+# ** Explicit/exact operations (non-fusible)
 
 eadd(x...) = Base.:+(x...)
 eadd(x::T, y::T) where {T<:NTuple} = SIMDPirates.evadd(x, y)
@@ -25,19 +57,19 @@ esub(x::T, y::T) where {T<:NTuple} = SIMDPirates.evsub(x, y)
 emul(x...) = Base.:*(x...)
 emul(x::T, y::T) where {T<:NTuple} = SIMDPirates.evmul(x, y)
 
+function explicit(expr::Expr)
+    replace_simd_ops(expr,
+                     (:+   => eadd,
+                      :-   => esub,
+                      :*   => emul,
+                      :fma => fma),
+                     (:+=  => eadd,
+                      :-=  => esub,
+                      :*=  => emul))
+end
+
 macro explicit(expr)
-    MacroTools.postwalk(expr) do x
-        x == :+   && return eadd
-        x == :-   && return esub
-        x == :*   && return emul
-        x == :fma && return fma
-
-        if MacroTools.@capture(x, a_ += b_)
-            return :($a = $eadd($a, $b))
-        end
-
-        return x
-    end |> esc
+    explicit(expr) |> esc
 end
 
 
@@ -53,19 +85,19 @@ fsub(x::T, y::T) where {T<:NTuple} = SIMDPirates.vsub(x, y)
 fmul(x...) = Base.:*(x...)
 fmul(x::T, y::T) where {T<:NTuple} = SIMDPirates.vmul(x, y)
 
+function fusible(expr::Expr)
+    replace_simd_ops(expr,
+                     (:+   => fadd,
+                      :-   => fsub,
+                      :*   => fmul,
+                      :fma => fma),
+                     (:+=  => fadd,
+                      :-=  => fsub,
+                      :*=  => fmul))
+end
+
 macro fusible(expr)
-    MacroTools.postwalk(expr) do x
-        x == :+   && return fadd
-        x == :-   && return fsub
-        x == :*   && return fmul
-        x == :fma && return fma
-
-        if MacroTools.@capture(x, a_ += b_)
-            return :($a = $fadd($a, $b))
-        end
-
-        return x
-    end |> esc
+    fusible(expr) |> esc
 end
 
 end
